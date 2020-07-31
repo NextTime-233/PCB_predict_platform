ed6673fc3a3bdf5d5ed743768c32e546b02a5816
Limlin-Qs <limlin95@126.com>
2020-07-31 14:30:55 +0800

    changed a few files deleted // "modules": false, from .babelrc

---
+++ src/assets/i18n/en.js
@@ -0,0 +1,12 @@
+/**
+ * 国际包 英文
+ * en_US
+ */
+module.exports = {
+    home:{
+        'title' : 'Management System',
+    },
+    footer:{
+        'title':'wink',
+    }
+}
---
+++ src/components/goods/shopping.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="_shopping">
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+._shopping {
+  height: 100%;
+  .img01 {
+    width: 99%;
+  }
+  .img05 {
+    width: 300px;
+    height: 350px;
+  }
+  .img06 {
+    width: 250px;
+    height: 400px;
+  }
+  .img07{
+    width: 250px;
+    height: 400px;
+  }
+  .tip{
+    color:red;
+  }
+}
+</style>
