22b15fcb0b3fb1b3aae819ff878ec119ea8e28a9
Limlin-Qs <limlin95@126.com>
2020-10-06 19:24:39 +0800

    接口地址修改102

--- src/views/UserAnalysis/Client.vue
+++ src/views/UserAnalysis/Client.vue
@@ -24,15 +24,29 @@
                                 </a-col>
                                 <a-col :md="6" >
                                     <a-form-item label="省份" :labelCol="{span: 8}" :wrapperCol="{span: 12, offset: 1}">
-                                        <a-input style="width: 100%" placeholder="请输入" size="small" v-model="clientForm.province"/>
+                                        <a-select v-model="clientForm.province" size="small" @change="handleProvinceChange">
+                                            <a-select-option v-for="province in provinceData" :key="province">
+                                                {{ province }}
+                                            </a-select-option>
+                                        </a-select>
                                     </a-form-item>
+<!--                                    <a-form-item label="省份" :labelCol="{span: 8}" :wrapperCol="{span: 12, offset: 1}">-->
+<!--                                        <a-input style="width: 100%" placeholder="请输入" size="small" v-model="clientForm.province"/>-->
+<!--                                    </a-form-item>-->
                                 </a-col>
                             </a-row>
                             <a-row v-if="advanced"  :gutter="8">
                                 <a-col :span="6" >
                                     <a-form-item label="城市" :labelCol="{span: 7}" :wrapperCol="{span: 12, offset: 1}">
-                                        <a-input placeholder="请输入" size="small" v-model="clientForm.city"/>
+                                        <a-select v-model="secondCity" size="small"  @change="handleCityChange">
+                                            <a-select-option v-for="city in cities" :key="city">
+                                                {{ city }}
+                                            </a-select-option>
+                                        </a-select>
                                     </a-form-item>
+                                        <!--                                    <a-form-item label="城市" :labelCol="{span: 7}" :wrapperCol="{span: 12, offset: 1}">-->
+<!--                                        <a-input placeholder="请输入" size="small" v-model="clientForm.city"/>-->
+<!--                                    </a-form-item>-->
                                 </a-col>
                                 <a-col :md="6" >
                                     <a-form-item
@@ -652,6 +666,46 @@
     </div>
 </template>
 <script>
+    const provinceData = ['河北省','山西省','辽宁省','吉林省','黑龙江省','江苏省','浙江省','安徽省','福建省','江西省','山东省',
+        '河南省','湖北省','湖南省','广东省','海南省','四川省','贵州省','云南省','陕西省','甘肃省','青海省','台湾省','内蒙古自治区',
+        '广西壮族自治区','西藏自治区','宁夏回族自治区','新疆维吾尔自治区','北京市','天津市','上海市','重庆市','香港特别行政区',
+        '澳门特别行政区'];
+    const cityData = {
+        河北省: ['','石家庄市','邯郸市','邢台市',' 保定市','张家口市',' 承德市',' 唐山市',' 秦皇岛市','沧州市','衡水市','廊坊市'],
+        山西省: ['','大同市','朔州市','忻州市','阳泉市','吕梁市','晋中市','长治市','晋城市','临汾市','运城市'],
+        辽宁省: ['','沈阳市','大连市','鞍山市','抚顺市','本溪市','丹东市','锦州市','营口市','阜新市','辽阳市','盘锦市','铁岭市','朝阳市','葫芦岛市'],
+        吉林省: ['','长春市','吉林市','四平市','通化市','白山市','辽源市','白城市','松原市'],
+        黑龙江省: ['','哈尔滨市','齐齐哈尔市','佳木斯市','鹤岗市','大庆市','鸡西市','双鸭山市','伊春市','牡丹江市','黑河市','七台河市','绥化市和大兴安岭地区'],
+        江苏省: ['','南京市','无锡市','徐州市','常州市','苏州市','南通市','连云港市','淮安市','扬州市','镇江市','泰州市','宿迁市'],
+        浙江省: ['','舟山市','杭州市','嘉兴市','温州市','宁波市','绍兴市','湖州市','丽水市','台州市','金华市','衢州市'],
+        安徽省: ['','宿州市','淮北市','亳州市','阜阳市','蚌埠市','淮南市','滁州市','六安市','马鞍山市','安庆市','芜湖市','铜陵市','宣城市','池州市','黄山市'],
+        福建省: ['','福州市','莆田市','泉州市','厦门市','漳州市','龙岩市','三明市','南平市','宁德市'],
+        江西省: ['','南昌市','九江市','鹰潭市','上饶市','新余市','景德镇市','宜春市','萍乡市','吉安市','抚州市','赣州市'],
+        山东省: ['','济南市','青岛市','淄博市','枣庄市','东营市','烟台市','潍坊市','济宁市','泰安市','威海市','日照市','滨州市','德州市','聊城市','临沂市','菏泽市','莱芜市'],
+        河南省: ['','郑州市','开封市','洛阳市','平顶山市','焦作市','鹤壁市','新乡市','安阳市','濮阳市','许昌市','漯河市','三门峡市','南阳市','商丘市','信阳市','周口市','驻马店市','济源市'],
+        湖北省: ['','武汉市','黄石市','十堰市','宜昌市','襄阳市','鄂州市','荆门市','孝感市','荆州市','黄冈市','咸宁市','随州市'],
+        湖南省: ['','长沙市','株洲市','湘潭市','衡阳市','邵阳市','岳阳市','常德市','张家界市','益阳市','郴州市','永州市','怀化市','娄底市','湘西土家族自治州'],
+        广东省: ['','珠海市','汕头市','佛山市','韶关市','湛江市','肇庆市','江门市','茂名市','惠州市','梅州市','汕尾市','河源市','阳江市','清远市','东莞市','中山市','潮州市','揭阳市','云浮市'],
+        海南省: ['','海口市','三亚市','三沙市','儋州市'],
+        四川省: ['','成都市','绵阳市','自贡市','攀枝花市','泸州市','德阳市','广元市','遂宁市','内江市','乐山市','资阳市','宜宾市','南充市','达州市','雅安市','广安市','巴中市','眉山市'],
+        贵州省: ['','贵阳市','六盘水市','遵义市','安顺市','铜仁市','毕节市'],
+        云南省: ['','昆明市','曲靖市','玉溪市','昭通市','保山市','丽江市','普洱市','临沧市'],
+        陕西省: ['','西安市','渭南市','咸阳市','汉中市','铜川市','宝鸡市','安康市','榆林市','商洛市','延安市'],
+        甘肃省: ['','兰州市','嘉峪关市','金昌市','白银市','天水市','武威市','张掖市','酒泉市','平凉市','庆阳市','定西市','陇南市','临夏回族自治州','甘南藏族自治州'],
+        青海省: ['','西宁市','海东市','海西蒙古族藏族自治州','海南藏族自治州','海北藏族自治州','黄南藏族自治州','果洛藏族自治州和玉树藏族自治州'],
+        台湾省: ['','台北市','新北市','台中市','高雄市','台南市'],
+        内蒙古自治区: ['','呼和浩特市','包头市','乌海市','赤峰市','通辽市','鄂尔多斯市','呼伦贝尔市','巴彦淖尔市','乌兰察布市'],
+        广西壮族自治区: ['','南宁市','崇左市','柳州市','来宾市','桂林市','梧州市','贺州市','玉林市','贵港市','百色市','钦州市','河池市','北海市','防城港市'],
+        西藏自治区: ['','拉萨市','日喀则市','昌都市','林芝市','山南市','那曲市'],
+        宁夏回族自治区: ['','银川市','石嘴山市','吴忠市','固原市','中卫市'],
+        新疆维吾尔自治区: ['','乌鲁木齐市','吐鲁番市','哈密市','克拉玛依市'],
+        北京市: [],
+        天津市: [],
+        上海市: [],
+        重庆市: [],
+        香港特别行政区: [],
+        澳门特别行政区: [],
+    };
     export default {
         name: "Client",
         data() {
@@ -678,10 +732,16 @@
                     PayDateEnd:'',
                 },
                 advanced: false,
+                // 日期范围
                 createValueB:[],
                 createValueP:[],
                 createValueS:[],
                 createValueR:[],
+                // limlin 2020.10.05 省份列表
+                provinceData,
+                cityData:[],
+                cities: cityData[provinceData[0]],
+                secondCity: cityData[provinceData[0]][0],
                 // 类型
                 cType: "客户类型",
                 rowData:{},
@@ -724,6 +784,7 @@
                 that.total = res.data.data  // 普通用户表
                 that.amount = res.data.data
             }).catch()
+
         },
         methods: {
             // 客户类型
@@ -782,6 +843,13 @@
                 this.clientForm.registrationTimeDateStart = dateString[0]
                 this.clientForm.registrationTimeDateEnd = dateString[1]
             },
+            // 下拉搜索
+            handleProvinceChange(value) {
+                console.log(value)
+                this.clientForm.province = value
+                this.cities = cityData[value];
+                this.secondCity = cityData[value][0];
+            },
             toggleAdvanced() {
                 this.advanced = !this.advanced
             },
@@ -799,7 +867,7 @@
                                     TotalPurchaseAmount: list.TotalPurchaseAmount,
                                     TotalPurchaseNum: list.TotalPurchaseNum,
                                     province: list.province,
-                                    city: list.city,
+                                    city: that.secondCity,
                                     LastPurchaseTimeDateStart: list.LastPurchaseTimeDateStart,
                                     LastPurchaseTimeDateEnd: list.LastPurchaseTimeDateEnd,
                                     PayDateStart: list.PayDateStart,
@@ -833,7 +901,7 @@
                                     TotalPurchaseAmount: list.TotalPurchaseAmount,
                                     TotalPurchaseNum: list.TotalPurchaseNum,
                                     province: list.province,
-                                    city: list.city,
+                                    city: that.secondCity,
                                     LastPurchaseTimeDateStart: list.LastPurchaseTimeDateStart,
                                     LastPurchaseTimeDateEnd: list.LastPurchaseTimeDateEnd,
                                     PayDateStart: list.PayDateStart,
@@ -890,6 +958,7 @@
                 that.createValueP = []
                 that.createValueR = []
                 that.createValueS = []
+                that.secondCity = ''
                 axios.get('backend/customer/listCustomers/1/5', {
                     headers: {
                         token: this.tokenStr
@@ -953,7 +1022,7 @@
                                 TotalPurchaseAmount: that.clientForm.TotalPurchaseAmount,
                                 TotalPurchaseNum: that.clientForm.TotalPurchaseNum,
                                 province: that.clientForm.province,
-                                city: that.clientForm.city,
+                                city: that.secondCity,
                                 LastPurchaseTimeDateStart: that.clientForm.LastPurchaseTimeDateStart,
                                 LastPurchaseTimeDateEnd: that.clientForm.LastPurchaseTimeDateEnd,
                                 PayDateStart: that.clientForm.PayDateStart,
@@ -989,7 +1058,7 @@
                                 TotalPurchaseAmount: that.clientForm.TotalPurchaseAmount,
                                 TotalPurchaseNum: that.clientForm.TotalPurchaseNum,
                                 province: that.clientForm.province,
-                                city: that.clientForm.city,
+                                city: that.secondCity,
                                 LastPurchaseTimeDateStart: that.clientForm.LastPurchaseTimeDateStart,
                                 LastPurchaseTimeDateEnd: that.clientForm.LastPurchaseTimeDateEnd,
                                 PayDateStart: that.clientForm.PayDateStart,
@@ -1031,7 +1100,7 @@
                                 TotalPurchaseAmount: that.clientForm.TotalPurchaseAmount,
                                 TotalPurchaseNum: that.clientForm.TotalPurchaseNum,
                                 province: that.clientForm.province,
-                                city: that.clientForm.city,
+                                city: that.secondCity,
                                 LastPurchaseTimeDateStart: that.clientForm.LastPurchaseTimeDateStart,
                                 LastPurchaseTimeDateEnd: that.clientForm.LastPurchaseTimeDateEnd,
                                 PayDateStart: that.clientForm.PayDateStart,
@@ -1067,7 +1136,7 @@
                                 TotalPurchaseAmount: that.clientForm.TotalPurchaseAmount,
                                 TotalPurchaseNum: that.clientForm.TotalPurchaseNum,
                                 province: that.clientForm.province,
-                                city: that.clientForm.city,
+                                city: that.secondCity,
                                 LastPurchaseTimeDateStart: that.clientForm.LastPurchaseTimeDateStart,
                                 LastPurchaseTimeDateEnd: that.clientForm.LastPurchaseTimeDateEnd,
                                 PayDateStart: that.clientForm.PayDateStart,
